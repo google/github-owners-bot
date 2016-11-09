91beefcbf4c271622c2a3871fce09fb5db99847d
erwinmombay <erwin.mombay@gmail.com>
2016-11-09 15:45:57 -0800

    update startup-script.sh

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-
-#   Copyright 2015-2016, The AMP HTML Authors.
+#   Copyright 2015-2016, Google, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,6 +14,16 @@
 
 # [START startup]
 set -v
+
+REPO="my/repo"
+APP_DIR="/opt/app"
+REPO_DIR="/opt/repo"
+GITHUB_BOT_USERNAME="mybotsname"
+SECRET_TOKEN="mysecrettoken"
+CLOUD_BUCKET="gs://my-cloud-bucket"
+GITHUB_ACCESS_TOKEN=""
+OAUTH2_CLIENT_ID=""
+OAUTH2_CLIENT_SECRET=""
 
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
@@ -41,29 +50,29 @@ ln -s /opt/nodejs/bin/npm /usr/bin/npm
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
 git config --global credential.helper gcloud.sh
-git clone https://source.developers.google.com/p/$PROJECTID /opt/app
+git clone https://source.developers.google.com/p/$PROJECTID "${APP_DIR}"
 
 # Install app dependencies
-cd /opt/app/
+cd "${APP_DIR}"
 npm install
 node_modules/gulp/bin/gulp.js babel
 
-git clone https://github.com/YOURPROJECT/REPO.git /opt/REPO
+git clone "https://github.com/${REPO}.git" "${REPO_DIR}"
 
 # Create a nodeapp user. The application will run as this user.
 useradd -m -d /home/nodeapp nodeapp
-chown -R nodeapp:nodeapp /opt/app
-chown -R nodeapp:nodeapp /opt/REPO
+chown -R nodeapp:nodeapp "${APP_DIR}"
+chown -R nodeapp:nodeapp "${REPO_DIR}"
 
 # Configure supervisor to run the node app.
 cat >/etc/supervisor/conf.d/node-app.conf << EOF
 [program:nodeapp]
-directory=/opt/app
+directory=${APP_DIR}
 command=npm start
 autostart=true
 autorestart=true
 user=nodeapp
-environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production",GCLOUD_PROJECT="",CLOUD_BUCKET="",DATA_BACKEND="datastore",OAUTH2_CLIENT_ID="",OAUTH2_CLIENT_SECRET="",GITHUB_ACCESS_TOKEN="",GITHUB_REPO_DIR="/opt/REPO",SECRET_TOKEN="secret",GITHUB_USERNAME=""
+environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production",GCLOUD_PROJECT="${PROJECTID}",CLOUD_BUCKET="${CLOUD_BUCKET}",DATA_BACKEND="datastore",OAUTH2_CLIENT_ID="${OAUTH2_CLIENT_ID}",OAUTH2_CLIENT_SECRET="${OAUTH2_CLIENT_SECRET}",GITHUB_ACCESS_TOKEN="${GITHUB_ACCESS_TOKEN}",GITHUB_REPO_DIR="${REPO_DIR}",SECRET_TOKEN="${SECRET_TOKEN}",GITHUB_USERNAME="${GITHUB_BOT_USERNAME}"
 stdout_logfile=syslog
 stderr_logfile=syslog
 EOF
