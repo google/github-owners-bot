e87c825cc1d82cc353228ae4e29ba450c5f88545
Erwin Mombay <erwin.mombay@gmail.com>
2019-02-12 12:53:02 -0800

    only allow prs from our fork temporarily

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ module.exports = app => {
   app.on(['pull_request.opened'], pullRequest)
 
   async function pullRequest(context) {
+    // Only allow PR's from our fork
+    if (!/repos\/erwinmombay/.test(context.payload.pull_request)) {
+      return;
+    }
     const pr = new PullRequest(context, context.payload.pull_request);
     await pr.processOpened();
   }
