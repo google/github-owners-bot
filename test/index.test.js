4b593ced5826e579883b34b9c1861f89fa439510
Erwin Mombay <erwin.mombay@gmail.com>
2019-02-12 10:37:39 -0800

    add author is also owner scenario

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,9 @@ const nock = require('nock')
 const owners = require('..')
 const {Probot} = require('probot')
 const payload = require('./fixtures/opened-4')
+const authorIsOwnerPayload = require('./fixtures/opened.author-is-owner')
 const filesPayload = require('./fixtures/files.35');
+const files36Payload = require('./fixtures/files.36');
 const reviewsPayload = require('./fixtures/reviews.35');
 const checkRunsPayload = require('./fixtures/check-runs.get.35');
 const emptyCheckRunsPayload = require('./fixtures/check-runs.get.35.empty');
@@ -82,38 +84,38 @@ describe('owners bot', () => {
       // We need the list of files on a pull request to evaluate the required
       // reviewers.
       nock('https://api.github.com')
-        .get('/repos/erwinmombay/github-owners-bot-test-repo/pulls/35/files')
-        .reply(200, filesPayload);
+        .get('/repos/erwinmombay/github-owners-bot-test-repo/pulls/36/files')
+        .reply(200, files36Payload);
 
       // We need the reviews to check if a pull request has been approved or
       // not.
       nock('https://api.github.com')
-        .get('/repos/erwinmombay/github-owners-bot-test-repo/pulls/35/reviews')
+        .get('/repos/erwinmombay/github-owners-bot-test-repo/pulls/36/reviews')
         .reply(200, reviewsPayload);
 
       nock('https://api.github.com')
-        .get('/repos/erwinmombay/github-owners-bot-test-repo/commits/ampprojectbot-patch-3/check-runs')
+        .get('/repos/erwinmombay/github-owners-bot-test-repo/commits/erwinmombay-patch-4/check-runs')
         .reply(200, emptyCheckRunsPayload);
 
-      // Test that a check-run is created
+      //// Test that a check-run is created
       nock('https://api.github.com')
         .post('/repos/erwinmombay/github-owners-bot-test-repo/check-runs', body => {
           expect(body).toMatchObject({
             name: 'AMP Owners bot',
-            head_branch: payload.pull_request.head.ref,
-            head_sha: payload.pull_request.head.sha,
+            head_branch: authorIsOwnerPayload.pull_request.head.ref,
+            head_sha: authorIsOwnerPayload.pull_request.head.sha,
             status: 'completed',
             conclusion: 'success',
             output: {
               title: 'AMP Owners bot check',
               summary: 'The check was a success!',
-              text: '# erwinmombay   - ./dir2/dir1/dir1/file.txt    ',
+              text: '# erwinmombay   - ./dir2/new-file.txt    ',
             }
           });
           return true;
         }).reply(200);
 
-      await probot.receive({event: 'pull_request', payload});
+      await probot.receive({event: 'pull_request', payload: authorIsOwnerPayload});
     });
   });
 
