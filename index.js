c0096d84b646da88eb839bf83f74e5e4392a2fd0
erwin mombay <erwin.mombay@gmail.com>
2019-05-14 15:18:16 -0700

    add basic Teams class (#67)
    also add missing copyright headers

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const PullRequest = require('./src/github').PullRequest;
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {PullRequest, Teams} = require('./src/github');
 
 module.exports = app => {
   app.on(['pull_request.opened', 'pull_request.synchronized'], onPullRequest)
@@ -13,6 +29,12 @@ module.exports = app => {
     level: process.LOG_LEVEL || 'info',
   });
 
+  async function processPullRequest(context, pullRequest) {
+    const pr = new PullRequest(context, pullRequest);
+    const teams = await new Teams(context).list();
+    return await pr.processOpened();
+  }
+
   async function onPullRequest(context) {
     return await processPullRequest(context, context.payload.pull_request);
   }
@@ -23,11 +45,6 @@ module.exports = app => {
       payload.repository.name, payload.check_run.check_suite.pull_requests[0].number);
 
     return await processPullRequest(context, pr.data);
-  }
-
-  async function processPullRequest(context, pullRequest) {
-    const pr = new PullRequest(context, pullRequest);
-    return await pr.processOpened();
   }
 
   async function onPullRequestReview(context) {
